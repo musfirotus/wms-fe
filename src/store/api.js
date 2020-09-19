584b0e97cb8c75b970d374426cf96b06417caf3d
musfirotus <musfirotus@gmail.com>
2020-09-19 19:15:05 +0700

    axios

--- src/store/api.js
+++ src/store/api.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 // const port = 3000;
 
+const token = localStorage.getItem("token");
+
+axios.defaults.headers.common = { Authorization: `bearer ${token}` };
+
 const instance = axios.create({
   baseURL: `https://simple-wms.herokuapp.com/api/v1/`,
   headers: {
