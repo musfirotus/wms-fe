8f16f84f5ad7a50ed86543b47b29587b80d57f42
musfirotus <musfirotus@gmail.com>
2020-09-20 14:25:41 +0700

    make it better

--- src/store/index.js
+++ src/store/index.js
@@ -2,22 +2,15 @@ import Vue from "vue";
 import Vuex from "vuex";
 import PersistedState from "vuex-persistedstate";
 
-// import Api from "./api";
 import Auth from "./auth";
 import Product from "./product";
 import User from "./user";
 import In from "./in";
 import Out from "./out";
 
-// import router from "../router/index";
-
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    getters: {},
     modules: {
         Auth,
         Product,
